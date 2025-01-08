3def06c4c4450a451f33da4d5997769fc3f29793
zatzoid <zatzoid@gmail.com>
2025-01-08 20:38:34 +0300

    zx

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,8 @@ import { rem } from './utils/constants'
 
 
 const HTML = $('html'),
-    SWIPE_SIZE = 100
+    SWIPE_SIZE = 100,
+    IS_MOBILE = window.innerWidth < 768
 $(function () {
     document.querySelectorAll('[data-anime-delay],[data-anime-speed]')
         .forEach((el) => {
@@ -26,7 +27,7 @@ $(function () {
     dropDowns()
     iniSwipers()
     mainPageCore()
-    sectionTopper()
+
 
 })
 function mainPageCore() {
@@ -69,9 +70,12 @@ function mainPageCore() {
                 }
             } else if (end + SWIPE_SIZE < touchStart) {
                 if (slide.dataset.animeState >= slide.dataset.animeStates) {
-                    swiper.mousewheel.enable()
-                    swiper.allowTouchMove = true
-                    swiper.slideNext()
+                    
+                    if(swiper.slides[swiper.activeIndex + 1]){
+                        swiper.mousewheel.enable()
+                        swiper.allowTouchMove = true
+                        swiper.slideNext()
+                    }
 
 
                 } else {
@@ -110,15 +114,15 @@ function mainPageCore() {
         })
         let wheelIsReady = true
         slide.addEventListener('wheel', (ev) => {
+
             if (!wheelIsReady) return
             wheelIsReady = false
-
             setTimeout(() => {
                 wheelIsReady = true
             }, 500);
-            console.log(ev.deltaY);
+
             if (ev.deltaY > 0) {
-               /*  console.log("Прокрутка вниз"); */
+                /*  console.log("Прокрутка вниз"); */
                 if (swiperSlider.activeIndex >= swiperSlider.slides.length - 1) {
                     swiperCore.mousewheel.enable()
                     swiperCore.allowTouchMove = true
@@ -130,7 +134,7 @@ function mainPageCore() {
             } else if (ev.deltaY < 0) {
                 /* console.log("Прокрутка вверх"); */
                 if (swiperSlider.activeIndex <= 0) {
-                    
+
                     swiperCore.mousewheel.enable()
                     swiperCore.allowTouchMove = true
                     swiperCore.slidePrev()
@@ -147,10 +151,8 @@ function mainPageCore() {
         modules: [Mousewheel, EffectCreative],
         direction: 'vertical',
         effect: 'creative',
-        creativeEffect: {
-
-        },
-        initialSlide: 9,
+        creativeEffect: {},
+        initialSlide: 10,
         followFinger: false,
         slidesPerView: 1,
         mousewheel: true,
@@ -171,17 +173,45 @@ function mainPageCore() {
                     })
                 swiper.wrapperEl.querySelectorAll('[data-anime-slider]')
                     .forEach((el) => {
+                        const cfg = {
+                            default: {
+                                modules: [Mousewheel],
+                                direction: 'vertical',
+                                spaceBetween: rem(3),
+                                slidesPerView: 'auto',
+                                mousewheel: false,
+                                simulateTouch: false
+                            }
+                        }
                         const slider = new Swiper(el.querySelector('.swiper'), {
-                            modules: [Mousewheel],
-                            direction: 'vertical',
-                            spaceBetween: rem(3),
+                            modules: [Mousewheel, Grid],
+                            slidesPerGroup: 2,
                             slidesPerView: 'auto',
+                            direction: 'vertical',
+                            grid: {
+                                fill: 'row',
+                            },
+                            spaceBetween: rem(3),
                             mousewheel: false,
                             simulateTouch: false
 
                         })
+
                         sectionSlider(swiper, slider, el)
                     })
+
+                if (IS_MOBILE) {
+
+                    if (swiper.slides[swiper.activeIndex].dataset.animeStates) {
+                        swiper.mousewheel.disable()
+                        swiper.allowTouchMove = false
+                    }
+                } else {
+                    if (swiper.slides[swiper.activeIndex].dataset.animeSlider) {
+                        swiper.mousewheel.disable()
+                        swiper.allowTouchMove = false
+                    }
+                }
             },
 
             slidePrevTransitionStart: (swiper) => {
@@ -208,6 +238,10 @@ function mainPageCore() {
                 swiper.slides[swiper.activeIndex].classList.remove('anime-over')
                 swiper.slides[swiper.activeIndex].classList.add('anime-start')
                 const activeSlide = swiper.slides[swiper.activeIndex]
+                if (activeSlide.classList.contains('section-with-topper')) {
+
+                    sectionTopper(activeSlide)
+                }
 
                 if (swiper.slides[swiper.activeIndex - 1] && swiper.slides[swiper.activeIndex - 1].dataset.animeStates) {
                     swiper.slides[swiper.activeIndex - 1].dataset.animeState = 1
@@ -279,7 +313,7 @@ function iniSwipers() {
             slidesPerView: 1,
             effect: 'fade',
             speed: 100,
-           
+
             followFinger: false,
             fadeEffect: {
                 crossFade: false
@@ -291,9 +325,9 @@ function iniSwipers() {
 
             }
         })
-       
 
-       
+
+
     }
 
     const results = document.querySelector('.results')
@@ -303,7 +337,7 @@ function iniSwipers() {
             effect: 'fade',
             speed: 100,
             followFinger: false,
-          
+
             fadeEffect: {
                 crossFade: false
             },
@@ -312,21 +346,12 @@ function iniSwipers() {
                 nextEl: results.querySelector('.swiper-btn-next')
             },
             slidesPerView: 1,
-            
+
         })
-       
+
     }
 
-    /*  const gallery = document.querySelector('.gallery')
-     if (gallery) {
-         new Swiper(gallery.querySelector('.swiper'), {
-             direction: 'vertical',
-             spaceBetween: rem(3),
-             slidesPerView: 'auto',
-             mousewheel: true
-         })
-     }
-  */
+
     const twoSlider = document.querySelector('.two-slider')
     if (twoSlider) {
         new Swiper(twoSlider.querySelector('.swiper'), {
@@ -382,6 +407,68 @@ function iniSwipers() {
         })
     }
 
+    const superAdv = document.querySelector('.supervisor-adv')
+    if (superAdv) {
+
+        const right = new Swiper(superAdv.querySelectorAll('.swiper')[1], {
+            modules: [EffectFade],
+            slidesPerView: 1,
+            effect: 'fade',
+            speed: 100,
+            initialSlide: 1,
+            followFinger: false,
+            fadeEffect: {
+                crossFade: false
+            },
+            simulateTouch: false,
+            allowTouchMove: false,
+            on: {
+                init: (s) => {
+                    s.slides.forEach((el, i) => {
+                        el.querySelector('.supervisor-adv__sliders-el-text-count')
+                            .textContent = (i + 1).toString().padStart(2, '0')
+                    })
+                }
+            }
+
+
+
+        })
+        const left = new Swiper(superAdv.querySelectorAll('.swiper')[0], {
+            modules: [Navigation, EffectFade],
+            slidesPerView: 1,
+            effect: 'fade',
+            speed: 100,
+            initialSlide: 0,
+            followFinger: false,
+            fadeEffect: {
+                crossFade: false
+            },
+            simulateTouch: false,
+            navigation: {
+                prevEl: superAdv.querySelector('.swiper-btn-prev'),
+                nextEl: superAdv.querySelector('.swiper-btn-next'),
+
+            },
+            on: {
+                slideChange: (s) => {
+                    right.slideTo(s.activeIndex + 1)
+                    if (window.innerWidth > 768 && s.activeIndex >= s.slides.length - 2) {
+                        s.allowSlideNext = false
+                    } else {
+                        s.allowSlideNext = true
+
+                    }
+                },
+                init: (s) => {
+                    s.slides.forEach((el, i) => {
+                        el.querySelector('.supervisor-adv__sliders-el-text-count')
+                            .textContent = (i + 1).toString().padStart(2, '0')
+                    })
+                }
+            }
+        })
+    }
 
 
 }
@@ -437,17 +524,13 @@ function dropDowns() {
 
 }
 
-function sectionTopper() {
-    const t = document.querySelectorAll('.section-with-topper')
-    if (!t) return
-    function click(e) {
-        e.target.closest('.section-with-topper').classList.toggle('_opened')
+function sectionTopper(target) {
+    if (!target) return
 
-    }
-    t.forEach((e) => {
-        e.querySelector('.section-with-topper__main')
-            .addEventListener('click', click, { once: true })
-    })
+    target.querySelector('.section-with-topper__main')
+        .addEventListener('click', (e) => {
+            target.classList.toggle('_opened')
+        }, { once: true })
 }
 function modalsHandler() {
     const modalOpeners = $('[data-modal]'),
